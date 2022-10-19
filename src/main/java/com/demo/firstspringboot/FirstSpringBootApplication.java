4ed3138d214f7a6f72fc4e36518afee4573833a4
Jonathan PC <chiqingyuan@hotmail.com>
2022-10-19 15:14:44 -0400

    Initial commit

---
+++ src/main/java/com/demo/firstspringboot/FirstSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.demo.firstspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+// all the config
+public class FirstSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirstSpringBootApplication.class, args);
+    }
+
+}
